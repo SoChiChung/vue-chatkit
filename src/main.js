d81779d40a7768fc36739a5beff7a412dae848f4
SoChichung <ddeadwings@gmail.com>
2019-11-14 00:34:12 +0800

    2019-11-14

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+/* import */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/loading.css'
+import './assets/css/loading-btn.css'
 Vue.config.productionTip = false
 
 new Vue({
